9ca32e6deefe739a728d3ea62edba5b3beb3f636
Finn <36964028+FinnRules@users.noreply.github.com>
2018-05-20 19:35:59 -0400

    Update enemies.py

--- UntitledOne/enemies.py
+++ UntitledOne/enemies.py
@@ -10,29 +10,31 @@ class Enemy:
 		return self.hp > 0
 	
 	def talk(self):
-		print("Not much for words")
+		print("\nNot much for words\n")
 
 class Guard(Enemy):
 	def __init__(self):
 		super().__init__(name='Guard', hp=10, damage=5)
-
+		self.aggro = True
 
 class LabTech(Enemy):
 	def __init__(self):
 		super().__init__(name='Lab Technician', hp=5, damage=2)
+		self.aggro = True
 
 class TunnelDweller(Enemy):
 	def __init__(self):
 		super().__init__(name='Tunnel Dweller', hp=5, damage=3)
 
 	def talk(self):
-		self.dialog = ["\nWho are you? Do you mean to hurt us?\n", "\nIn that case you are welcome, but disturb nothing\n"]
-		self.fightwords = ["\nWe will defend the tunnel at all costs!\n"]
+		self.dialog = ["\nTunnel Dweller: Who are you? Do you mean to hurt us?\n", "\nTunnel Dweller: In that case you are welcome, but disturb nothing\n"]
+		self.fightwords = ["\nTunnel Dweller: We will defend the tunnel at all costs!\n"]
 
 		print(self.dialog[0])
 		response = input('y/n: ')
 		if response != 'n':
-			print(self.fightwords)
+			print(self.fightwords[0])
 			self.aggro = True
 			return
 		print(self.dialog[1])
+		return

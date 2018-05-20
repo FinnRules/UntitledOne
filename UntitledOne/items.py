4fbc54513eed0eaadd886fbf41516ad7fc861465
Finn <36964028+FinnRules@users.noreply.github.com>
2018-05-20 11:55:37 -0400

    Update items.py

--- UntitledOne/items.py
+++ UntitledOne/items.py
@@ -40,5 +40,5 @@ class StartingClosetPaper(Item):
 		super().__init__(name="Paper", description="A notice of some sort", value=1)
 
 	def use_item(self):
-		print("""\nRed Sector Closet Borrowing Sheet:\n3/5: Harry Allen\n3/6: Harry Allen\n3/7: Harry Allen\n3/8: Harry Allen\n3/9: Harry Al\n3/10: Harry Alfred<The rest of the sheet is obscurred with heavy pen scribbles""")
+		print("""\nRed Sector Closet Borrowing Sheet:\n3/5: Harry Allen\n3/6: Harry Allen\n3/7: Harry Allen\n3/8: Harry Allen\n3/9: Harry Al\n3/10: Harry Alfred\n<The rest of the sheet is obscurred with heavy pen scribbles""")
 
